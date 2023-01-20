eb6ba8ff7c2267171359f238e47af2b0c17c5a01
Mateusz <mateusz.grzybowski98@gmail.com>
2023-01-20 13:57:05 +0100

    Created keyboard funcionality

--- src/App.tsx
+++ src/App.tsx
@@ -7,24 +7,47 @@ import { boardDefault } from './Words';
 interface AppContextProps {
   board: string[][];
   setBoard: React.Dispatch<React.SetStateAction<string[][]>>;
+  currentAttempt: {
+    attempt: number;
+    letterPos: number;
+  };
+  setCurrentAttempt: React.Dispatch<
+    React.SetStateAction<{
+      attempt: number;
+      letterPos: number;
+    }>
+  >;
 }
 
 export const AppContext = createContext<AppContextProps>({
   board: boardDefault,
   setBoard: () => {},
+  currentAttempt: {
+    attempt: 0,
+    letterPos: 0,
+  },
+  setCurrentAttempt: () => {},
 });
 
 function App() {
   const [board, setBoard] = useState(boardDefault);
+  const [currentAttempt, setCurrentAttempt] = useState({
+    attempt: 0,
+    letterPos: 0,
+  });
 
   return (
     <div className="App">
       <nav className="navigation">
         <h1 className="title">Wordle</h1>
       </nav>
-      <AppContext.Provider value={{ board, setBoard }}>
-        <Board />
-        <Keyboard />
+      <AppContext.Provider
+        value={{ board, setBoard, currentAttempt, setCurrentAttempt }}
+      >
+        <div className="game">
+          <Board />
+          <Keyboard />
+        </div>
       </AppContext.Provider>
     </div>
   );
---
+++ src/components/Key.tsx
@@ -0,0 +1,40 @@
+import React, { useContext } from 'react';
+import { AppContext } from '../App';
+
+interface KeyProps {
+  keyVal: string;
+  bigKey?: boolean;
+}
+
+const Key = ({ keyVal, bigKey }: KeyProps) => {
+  const { board, setBoard, currentAttempt, setCurrentAttempt } =
+    useContext(AppContext);
+
+  const selectLetter = () => {
+    if (keyVal === 'ENTER') {
+      if (currentAttempt.letterPos !== 5) return;
+      setCurrentAttempt({
+        attempt: currentAttempt.attempt + 1,
+        letterPos: 0,
+      });
+    } else {
+      if (currentAttempt.letterPos > 4) return;
+      const newBoard = [...board];
+      newBoard[currentAttempt.attempt][currentAttempt.letterPos] = keyVal;
+
+      setBoard(newBoard);
+      setCurrentAttempt({
+        ...currentAttempt,
+        letterPos: currentAttempt.letterPos + 1,
+      });
+    }
+  };
+
+  return (
+    <div className="key" id={bigKey ? 'big' : ''} onClick={selectLetter}>
+      {keyVal}
+    </div>
+  );
+};
+
+export default Key;
